10671ff1c6ab28c8617e65a3f5217ea7060bd89d
Gieroj <gieroj@gmail.com>
2020-05-06 00:11:14 -0500

    Start working on the package.

---
+++ src/BarchartApiServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Gieroj\BarchartApi;
+
+use Illuminate\Support\ServiceProvider;
+
+class BarchartApiServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
